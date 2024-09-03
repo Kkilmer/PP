0ec2f1405a91653b42fe1b31995370c59832d8ea
KevinK <kp827767@gmail.com>
2024-09-02 21:07:05 -0300

    bee's exer.

--- src/tech/kevin/ex/Bee1070.java
+++ src/tech/kevin/ex/Bee1070.java
@@ -1,17 +1,15 @@
 package tech.kevin.ex;
-
 import java.util.Scanner;
 
-public class Bee1067 {
+public class Bee1070 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-
         int valorX = leitor.nextInt();
-        for (int i = 1; i <= valorX; i++) {
+
+        for (int i = 1; i <= valorX; i++){
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
 }
-
